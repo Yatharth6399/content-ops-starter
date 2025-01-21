dadbc513c2d057dc4b328d93ca5269ac57939026
Yatharth6399 <108324495+Yatharth6399@users.noreply.github.com>
2025-01-21 21:57:25 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import Script from 'next/script';
-import '../styles/globals.css'; // Your CSS imports if any
+import '../styles/main.css'; // Your CSS imports if any
 
 export default function App({ Component, pageProps }) {
   return (
